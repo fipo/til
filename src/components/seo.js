8735bb70a905028cb05ceff77d53f0e590f7a211
Philip Sotirov <me@philipsotirov.com>
2019-02-27 15:15:53 +0200

    Add google-site-verification

--- src/components/seo.js
+++ src/components/seo.js
@@ -60,6 +60,10 @@ function SEO({ description, lang, meta, keywords, title }) {
                 name: 'twitter:description',
                 content: metaDescription,
               },
+              {
+                name: 'google-site-verification',
+                content: 'IqAIRt96l_n1mzvYO-w9ph73bl_gSSlT6jJ9c_XRf5c'
+              }
             ]
               .concat(
                 keywords.length > 0
